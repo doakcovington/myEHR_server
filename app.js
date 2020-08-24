3d7fc4d312b8f0066bc6e6204f0f92e0f659617f
doakcovington <doakcovington@protonmail.com>
2020-08-24 12:35:37 -0700

    Built out database connection

--- app.js
+++ app.js
@@ -3,6 +3,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('myehr', 'postgres', 'admin', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+sequelize.authenticate()
+    .then(() => console.log("Connected to myehr database"))
+    .catch(error => console.log(error.message))
+
 
 const app = express();
 
@@ -11,3 +22,4 @@ app.get('/', (request, response) => response.send("We Don't Go To Ravenholm"));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
